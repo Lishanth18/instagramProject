b328e7c7653696a4142ac2fa5fc965dfa4587b39
Lishanth18 <lishanth5072@gmail.com>
2025-10-29 11:11:07 +0530

    Initial commit - added react project files

---
+++ Instagram/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='d-flex'>
+      <div className='w-20'>SideBar</div>
+      <div className='w-50'>Feed</div>
+      <div className='w-30'>Suggestion</div>
+    </div>
+  )
+}
+
+export default App
